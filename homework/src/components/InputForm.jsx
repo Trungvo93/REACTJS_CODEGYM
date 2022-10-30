b3a6a26239d1f350078bd7be4906fea254b3d240
Trung <thanhtrungvo1993@gmail.com>
2022-10-30 22:49:09 +0700

    ,

---
+++ homework/src/components/InputForm.jsx
@@ -0,0 +1,6 @@
+export const InputForm = (props) =>{
+    return <>
+        <label htmlFor={props.id}>{props.label}</label>
+        <input type={props.type} name={props.name} value={props.value} onChange={props.onChange} id={props.id}/>
+    </>
+}
