d288d38bf37a21c25970566d9b1981149a99aa67
Trungvo93 <100949580+Trungvo93@users.noreply.github.com>
2022-11-03 22:02:21 +0700

    .

--- demorouter/src/components/router2/Product.jsx
+++ demorouter/src/components/router2/Product.jsx
@@ -1,6 +1,6 @@
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 export const Product = () => {
-
+    const navigate =  useNavigate();
     let { categoryID, randomUrl } = useParams();
     console.log(randomUrl)
     return <div>
@@ -8,8 +8,9 @@ export const Product = () => {
             ID Selected: {categoryID}
         </h3>
         <h2>Random id: {randomUrl}</h2>
-        <button>
-            <Link to='/'>Back</Link>
+        {/* Quay lại trang trước */}
+        <button onClick={()=>{navigate(-1)}}>
+            Back
         </button>
     </div>
 }
