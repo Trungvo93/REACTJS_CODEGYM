6aa07d7f2ec1a9d71a4ad359299bfae3228e26d0
Trungvo93 <100949580+Trungvo93@users.noreply.github.com>
2022-11-09 01:27:25 +0700

    .

--- homeworknextjs/pages/components/weather/ShowWeather.jsx
+++ homeworknextjs/pages/components/weather/ShowWeather.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { Formik, Form, Field } from "formik";
 import LayoutWeather from "./LayoutWeather";
 
 const ShowWeather = () => {
-  const [form, setForm] = useState("");
   const [weather, setWeather] = useState({
     temp: "",
     humidity: "",
@@ -12,14 +10,10 @@ const ShowWeather = () => {
     wind: "",
     visibility: "",
   });
-  const handleChange = (e) => {
-    setForm(e.target.value);
-  };
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleChange = async (e) => {
     try {
       const res = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${form}&units=metric&appid=06b48d7f898665ae92a12aa3908c5a2e`
+        `https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&units=metric&appid=06b48d7f898665ae92a12aa3908c5a2e`
       );
       console.log(res.data);
       setWeather({
@@ -36,24 +30,60 @@ const ShowWeather = () => {
 
   return (
     <LayoutWeather>
-      <div>
-        <h1>showWeather</h1>
-        <Formik
-          initialValues={form}
-          enableReinitialize={true}
-          onSubmit={handleSubmit}>
-          <Form>
-            <Field
-              type="text"
-              name="city"
-              value={form || ""}
-              onChange={handleChange}></Field>
-            <button type="submit" onClick={handleSubmit}>
-              Submit
-            </button>
-          </Form>
-        </Formik>
-        <div>
+      <div className="row p-5">
+        <div className="col">
+          <h1>Thời tiết tỉnh thành Việt Nam</h1>
+          <select defaultValue="default" onChange={handleChange}>
+            <option value="default" disabled hidden>
+              -- Choose --
+            </option>
+            <option value="An Giang">An Giang</option>
+            <option value="Bà Rịa">Bà Rịa - Vũng Tàu</option>
+            <option value="Bình Định">Bình Định</option>
+            <option value="Bạc Liêu">Bạc Liêu</option>
+            <option value="Bắc Giang">Bắc Giang</option>
+            <option value="Bắc Kạn">Bắc Kạn</option>
+            <option value="Bắc Ninh">Bắc Ninh</option>
+            <option value="Bến Tre">Bến Tre</option>
+            <option value="Cao Bằng">Cao Bằng</option>
+            <option value="Cà Mau">Cà Mau</option>
+            <option value="Cần Thơ">Cần Thơ</option>
+            <option value="Gia Lai">Gia Lai</option>
+            <option value="Hà Giang">Hà Giang</option>
+            <option value="Hà Nam">Hà Nam</option>
+            <option value="Hà Nội">Hà Nội</option>
+            <option value="Hà Tĩnh">Hà Tĩnh</option>
+            <option value="Hưng Yên">Hưng Yên</option>
+            <option value="Hai Duong">Hải Dương</option>
+            <option value="Hải Phòng">Hải Phòng</option>
+            <option value="Ho Chi Minh">TP. Hồ Chí Minh</option>
+            <option value="Khánh Hòa">Khánh Hòa</option>
+            <option value="Kon Tum">Kon Tum</option>
+            <option value="Lai Châu">Lai Châu</option>
+            <option value="Long An">Long An</option>
+            <option value="Lào Cai">Lào Cai</option>
+            <option value="Lạng Sơn">Lạng Sơn</option>
+            <option value="Nam Định">Nam Định</option>
+            <option value="Ninh Bình">Ninh Bình</option>
+            <option value="Phú Thọ">Phú Thọ</option>
+            <option value="Quảng Ngãi">Quảng Ngãi</option>
+            <option value="Quảng Trị">Quảng Trị</option>
+            <option value="Sóc Trăng">Sóc Trăng</option>
+            <option value="Sơn La">Sơn La</option>
+            <option value="Thanh Hóa">Thanh Hóa</option>
+            <option value="Thái Bình">Thái Bình</option>
+            <option value="Thai Nguyen">Thái Nguyên</option>
+            <option value="Hue">Huế</option>
+            <option value="Trà Vinh">Trà Vinh</option>
+            <option value="Tuyên Quang">Tuyên Quang</option>
+            <option value="Tay Ninh">Tây Ninh</option>
+            <option value="Vĩnh Long">Vĩnh Long</option>
+            <option value="Vĩnh Phúc">Vĩnh Phúc</option>
+            <option value="Yên Bái">Yên Bái</option>
+            <option value="Da Nang">Đà Nẵng</option>
+          </select>
+        </div>
+        <div className="col">
           <p>Nhiệt độ: {weather.temp} độ C</p>
           <p>Độ ẩm: {weather.humidity}%</p>
           <p>Trời: {weather.weather}</p>
@@ -61,6 +91,7 @@ const ShowWeather = () => {
           <p>Tầm nhìn: {weather.visibility / 1000}km</p>
         </div>
       </div>
+      <div></div>
     </LayoutWeather>
   );
 };
--- homeworknextjs/pages/index.js
+++ homeworknextjs/pages/index.js
@@ -2,8 +2,7 @@ import Link from "next/link";
 import InfoCovid from "./components/covid/infoCovid";
 import ListCovid from "./components/covid/listCovid";
 import Layout from "./components/toolbar/layout";
-import LayoutWeather from "./components/weather/layoutWeather";
-import ShowWeather from "./components/weather/showWeather";
+import ShowWeather from "./components/weather/ShowWeather";
 
 export default function Home() {
   return <ShowWeather></ShowWeather>;
